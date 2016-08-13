67d612c7921dbd407e2e787d515e5148812dcbc6
Nathan Cooper <nac33@students.uwf.edu>
2016-08-13 09:37:53 -0500

    Moved run.sh to Student's repo

---
+++ run.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+echo "Hello World"
+
+# Running phpunit tests
+phpunit /pipeline/Tester.php
+
+#echo "____________________"
+#echo "____________________"
+
+#phpcs /pipeline/Person.php
+
+#echo "____________________"
+#echo "____________________"
+
+#phpcs /pipeline/Tester.php
+
+#echo "____________________"
+#echo "____________________"
+
+#python -m SimpleHTTPServer 8080
