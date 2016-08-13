8b6defec59267459c2aa201309b1d9b63d00c126
Nathan Cooper <nac33@students.uwf.edu>
2016-08-13 11:30:25 -0500

    Testing successful build

--- Tester.php
+++ Tester.php
@@ -6,7 +6,7 @@ class Tester extends PHPUnit_Framework_TestCase{
         public $test;
 
         public function testName(){
-                $this->test=new Person("Jason1");
+                $this->test=new Person("Jason");
                 $this->assertEquals($this->test->getName(),"Jason");
         }
 }
