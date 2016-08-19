d23b0641e8c45219f27b0882cc2c16a245b59714
Nathan Cooper <nac33@students.uwf.edu>
2016-08-19 13:40:46 -0500

    Added app php files and tests

--- Gadget.php
+++ Gadget.php
@@ -1,33 +1,73 @@
 <?php
 
+
 class Gadget {
+
+	public $item;
        	public $name;
        	public $model;
 	public $cost;
        	public $manufacturer;
-       	public $functionality;
+       	public $specifications;
+
+	public function __construct($item){
+		$this->item = $item;
+		$this->setName();
+		$this->setModel();
+		$this->setCost();
+		$this->setManufacturer();
+		$this->setSpecifications();
+	}
 
 
 
-       	public function setName($Name){
-		$this->name = $Name;
+
+	public function fileContent($num)
+	{
+		$tempArray = array();
+		$temp = file_get_contents('gadgetinfo.txt');
+		$tempArray = explode("|", $temp);
+		return $tempArray[$num];
+	}
+	
+	public function getField($num)
+	{	
+		$fieldArray = array();
+		$temp = $this->fileContent($this->item);
+		$fieldArray = explode(";", $temp);
+		return $fieldArray[$num];
+	}
+
+
+
+
+
+
+
+       	public function setName(){
+		$this->name = $this->getField(0);
 	} 
         
-        public function setModel($Model){
-               	$this->model = $Model;
+     	public function setManufacturer(){
+                $this->manufacturer = $this->getField(1);
+        } 
+
+	public function setCost(){
+                $this->cost =  $this->getField(2);
+        } 
+
+	
+        public function setModel(){
+               	$this->model = $this->getField(3);
        	} 
 
-	public function setCost($Cost){
-               	$this->cost = $Cost;
-       	} 
         
-	public function setManufacturer($Manufacturer){
-               	$this->manufacturer = $Manufacturer;
-       	} 
-        
-	public function setFunctionality($Functionality){
-               	$this->functionality = $Functionality;
+	public function setSpecifications(){
+               	$this->specifications = $this->getField(4);
        	}
+
+
+
 
 
 
@@ -35,26 +75,21 @@ class Gadget {
                return $this->name;
         } 
 
-       	public function getModel($Model){
+       	public function getModel(){
                 return $this->model;
         } 
 
-       	public function getCost($Cost){
+       	public function getCost(){
                 return $this->cost;
-        } 
-       	
-        public function getManufacturer($Manufacturer){
-                return $this->manufacturer;
-        } 
-	
-        public function getFunctionality($Functionality){
-               	return $this->functionality;
-       	} 
+        }
 
-	public function gImages(){
-		public $link = www.google.com;
-		return $link;
-	}
+        public function getManufacturer(){
+                return $this->manufacturer;
+        }
+
+        public function getSpecifications(){
+               	return $this->specifications;
+       	}
 
 }
 
