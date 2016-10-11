f10bd3f2a00a9463ce6126c8ef7aa02ed05997e6
Nathan Cooper <nac33@students.uwf.edu>
2016-10-11 14:04:04 -0500

    Welcoming amazon

--- index/index.php
+++ index/index.php
@@ -11,7 +11,7 @@
 <body>
 	<div id="big_wrapper">
 
-	<header id="header">Welcome amazon people!</header>
+	<header id="header">Hello Amazon</header>
 	<nav id="navigation">
 		<ul>Home</ul>
                 <ul>About</ul>
