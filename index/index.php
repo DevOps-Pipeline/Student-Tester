e56f9a3c84c6d037a0846d79dd79ae36ea2ab703
Nathan Cooper <nac33@students.uwf.edu>
2016-10-11 15:02:22 -0500

    commit

--- index/index.php
+++ index/index.php
@@ -11,7 +11,7 @@
 <body>
 	<div id="big_wrapper">
 
-	<header id="header">Hi there</header>
+	<header id="header">Hi!!!</header>
 	<nav id="navigation">
 		<ul>Home</ul>
                 <ul>About</ul>
