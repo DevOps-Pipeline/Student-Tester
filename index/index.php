b3e0d1633e327ffca99d5fe1bb1ae3cfd865dda6
Nathan Cooper <nac33@students.uwf.edu>
2016-10-11 14:56:57 -0500

    amazon

--- index/index.php
+++ index/index.php
@@ -11,7 +11,7 @@
 <body>
 	<div id="big_wrapper">
 
-	<header id="header">Hello There!</header>
+	<header id="header">Hi</header>
 	<nav id="navigation">
 		<ul>Home</ul>
                 <ul>About</ul>
