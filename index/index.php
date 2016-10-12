b48b385aa68a7d9836540571a7f4ed0297be4a86
Nathan Cooper <nac33@students.uwf.edu>
2016-10-12 18:06:32 -0500

    iten

--- index/index.php
+++ index/index.php
@@ -11,7 +11,7 @@
 <body>
 	<div id="big_wrapper">
 
-	<header id="header">Hello!!x</header>
+	<header id="header">Hello World!!x</header>
 	<nav id="navigation">
 		<ul>Home</ul>
                 <ul>About</ul>
