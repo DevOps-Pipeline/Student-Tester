c235a484b749f54778422321e7fe37338c771416
Nathan Cooper <nac33@students.uwf.edu>
2016-10-26 20:41:22 -0500

    Test of new build labeling system

--- index/index.php
+++ index/index.php
@@ -11,7 +11,7 @@
 <body>
 	<div id="big_wrapper">
 
-	<header id="header">GADGET INVENTORY TEST</header>
+	<header id="header">GADGET INVENTORY</header>
 	<nav id="navigation">
 		<ul>Home</ul>
                 <ul>About</ul>
