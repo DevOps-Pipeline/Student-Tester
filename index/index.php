07091699160b7d823118e6b3f4c8ee63e1f85301
Nathan Cooper <nac33@students.uwf.edu>
2016-11-01 22:53:07 -0500

    Remove undefined methods

--- index/index.php
+++ index/index.php
@@ -30,8 +30,8 @@
 ?>
         <aside id="left_side">
                 <ul><b>Type </b><br/><p><?php echo $G->getName(); ?></p></ul>
-                <ul><b>Manufacturer </b><br/><p><?php echo $G->getManufacturer(); ?></p></ul>
-                <ul><b>Cost </b><br/><p><?php echo $G->getCost(); ?><p></ul>
+                <!--<ul><b>Manufacturer </b><br/><p><?php echo $G->getManufacturer(); ?></p></ul>
+                <ul><b>Cost </b><br/><p><?php echo $G->getCost(); ?><p></ul>-->
                 <ul><b>Amazon </b><br/><a href="<?php $G->getLink(); ?>"><?php echo $G->getModel(); ?></a></ul>
 
         </aside>
