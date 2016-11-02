aedcc6c5ba3e01ffe68d056552e932e608b7e10a
Nathan Cooper <nac33@students.uwf.edu>
2016-11-01 22:46:23 -0500

    Remove unneeded assertions

--- tests/localTester.php
+++ tests/localTester.php
@@ -9,14 +9,6 @@ class localTester extends PHPUnit_Framework_TestCase{
             $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "", "1.jpg", "res1.jpg");
                 $this->assertEquals($this->test->getName(), "Gaming Laptop");
         }
-        public function testManufacturer(){
-            $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "https://www.amazon.com/Alienware-M17X-17-3-Black-Laptop/dp/B0054Y5X36", "1.jpg", "res1.jpg");
-                $this->assertEquals($this->test->getManufacturer(), "Alienware");
-        }
-        public function testCost(){
-            $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "https://www.amazon.com/Alienware-M17X-17-3-Black-Laptop/dp/B0054Y5X36", "1.jpg", "res1.jpg");
-                $this->assertEquals($this->test->getCost(), "$699");
-        }
         public function testModel(){
             $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "https://www.amazon.com/Alienware-M17X-17-3-Black-Laptop/dp/B0054Y5X36", "1.jpg", "res1.jpg");
                 $this->assertEquals($this->test->getModel(), "Alienware-M17X");
