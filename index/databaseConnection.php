ed6401036d0370291e4473f9d6ef91fbd9545afb
Nathan Cooper <nac33@students.uwf.edu>
2016-11-03 21:38:54 -0500

    Restructure and add comments to databaseConnection.php

--- index/databaseConnection.php
+++ index/databaseConnection.php
@@ -9,47 +9,78 @@ class databaseConnection {
 	public $dbuser;
 	public $dbpass;
 	public $conn;
-	public function __construct(){
+
+	/** Setting up the different properties needed to connect to the database */
+	public function __construct()
+	{
+
 	        $this->dbhost = 'db:3306';
 	        $this->dbname = 'Tester';
         	$this->dbuser = 'naman';
         	$this->dbpass = 'chocolate';
+
 	}
-	public function connect(){
-        try {
-                $this->conn = new PDO("mysql:host=$this->dbhost;dbname=$this->dbname", $this->dbuser, $this->dbpass);
-                }catch(Exception $e){
+
+	/** Trying to connect to the database */
+	public function connect()
+	{
+
+	    try
+		{
+
+	        $this->conn = new PDO("mysql:host=$this->dbhost;dbname=$this->dbname", $this->dbuser, $this->dbpass);
+
+        }
+		catch(Exception $e)
+		{
+
 			echo "Error: " . $e->getMessage();
+
 		}
+
 	}
 
-	public function fetch($id){
-       		$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-       		$sql = $this->conn->prepare("SELECT * FROM gadget where srno=$id");
-       		$sql->execute();
-       		$result = $sql->fetchAll();
-  		return $result;
+	/** Sending a query to the database */
+	public function fetch($id)
+	{
+
+		$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = $this->conn->prepare("SELECT * FROM gadget where srno=$id");
+		$sql->execute();
+		$result = $sql->fetchAll();
+		return $result;
+
 	}
 
+	/** Creating gadgets based on the entries in the database */
+	public function createGadget($id)
+	{
 
-	public function createGadget($id){
-		foreach($this->fetch($id) as $row){
-			$gadget = new Gadget(	$row['srno'],
-						$row['Type'],
-						$row['Name'],
-						$row['Cost'],
-						$row['Brand'],
-						$row['Link'],
-						$row['imgPath'],
-						$row['icon']	);
+		foreach($this->fetch($id) as $row)
+		{
+
+			$gadget = new Gadget($row['srno'],
+								 $row['Type'],
+								 $row['Name'],
+								 $row['Cost'],
+								 $row['Brand'],
+								 $row['Link'],
+								 $row['imgPath'],
+								 $row['icon']);
+
 		}
+
 		return $gadget;
 
 	}
 
-	public function getGadget($ID){
+	/** Returns the newly created gadget */
+	public function getGadget($ID)
+	{
+
 		return $this->createGadget($ID);
 
 	}
+
 }
 ?>
