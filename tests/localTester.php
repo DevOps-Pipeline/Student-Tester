bca314f990589fd08e0896eddf2499542d8f343d
Nathan Cooper <nac33@students.uwf.edu>
2016-11-03 21:13:26 -0500

    Restructure unit tests

--- tests/localTester.php
+++ tests/localTester.php
@@ -2,12 +2,15 @@
 
 require_once '/pipeline/index/Gadget.php';
 
-class localTester extends PHPUnit_Framework_TestCase{
-    public $test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "", "1.jpg", "res1.jpg");
+class localTester extends PHPUnit_Framework_TestCase
+{
+
+    public $test;
 
     public function testName()
     {
 
+        $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "www.amazon.com", "1.jpg", "res1.jpg");
         $this->assertEquals($this->test->getName(), "Gaming Laptop");
 
     }
@@ -16,25 +19,45 @@ class localTester extends PHPUnit_Framework_TestCase{
 
     /** Implement the testManufacturer method below */
 
-    public function testModel(){
-        $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "https://www.amazon.com/Alienware-M17X-17-3-Black-Laptop/dp/B0054Y5X36", "1.jpg", "res1.jpg");
-            $this->assertEquals($this->test->getModel(), "Alienware-M17X");
+    public function testModel()
+    {
+
+        $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "www.amazon.com", "1.jpg", "res1.jpg");
+        $this->assertEquals($this->test->getModel(), "Alienware-M17X");
+
     }
-    public function testLink(){
-        $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "https://www.amazon.com/Alienware-M17X-17-3-Black-Laptop/dp/B0054Y5X36", "1.jpg", "res1.jpg");
-            $this->assertEquals($this->test->getLink(), "https://www.amazon.com/Alienware-M17X-17-3-Black-Laptop/dp/B0054Y5X36");
+
+    public function testLink()
+    {
+
+        $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "www.amazon.com", "1.jpg", "res1.jpg");
+        $this->assertEquals($this->test->getLink(), "www.amazon.com");
+
     }
-    public function testImage(){
-        $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "https://www.amazon.com/Alienware-M17X-17-3-Black-Laptop/dp/B0054Y5X36", "1.jpg", "res1.jpg");
-            $this->assertEquals($this->test->getImageName(), "1.jpg");
+
+    public function testImage()
+    {
+
+        $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "www.amazon.com", "1.jpg", "res1.jpg");
+        $this->assertEquals($this->test->getImageName(), "1.jpg");
+
     }
-    public function testIcon(){
-        $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "https://www.amazon.com/Alienware-M17X-17-3-Black-Laptop/dp/B0054Y5X36", "1.jpg", "res1.jpg");
-            $this->assertEquals($this->test->getIcon(), "res1.jpg");
+
+    public function testIcon()
+    {
+
+        $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "www.amazon.com", "1.jpg", "res1.jpg");
+        $this->assertEquals($this->test->getIcon(), "res1.jpg");
+
     }
-    public function testItem(){
-        $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "https://www.amazon.com/Alienware-M17X-17-3-Black-Laptop/dp/B0054Y5X36", "1.jpg", "res1.jpg");
-            $this->assertEquals($this->test->getItem(), 1);
+
+    public function testItem()
+    {
+
+        $this->test = new Gadget(1, "Gaming Laptop", "Alienware-M17X", "$699", "Alienware", "www.amazon.com", "1.jpg", "res1.jpg");
+        $this->assertEquals($this->test->getItem(), 1);
+
     }
+    
 }
 ?>
